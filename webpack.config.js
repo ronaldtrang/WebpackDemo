91b39c2d56b531dadb30a99d67dfe56fbac5025d
v-rotran <v-rotran@RTRANG>
2017-01-23 17:03:04 -0800

    Separate CSS bundle and Auto Prefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 ﻿var path = require('path');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     context: path.resolve('js'),
     entry: ['./app'],
     output: {
-        path: path.resolve('build/js'), // This is a virtual path
-        publicPath: '/public/assets/js/', // Physical path for the bundle.js stated in the index.html
+        path: path.resolve('build/'), // This is a virtual path where the bundle files exist
+        publicPath: '/public/assets/', // Physical path for the bundle.js stated in the index.html
         filename: 'bundle.js' 
     },
-
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ],
     devServer: {
         contentBase: 'public'
     },
@@ -18,7 +22,12 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                loader: 'style-loader!css-loader'
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader')
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!sass-loader')
             }
         ]
     },
